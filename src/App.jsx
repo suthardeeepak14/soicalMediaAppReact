51dc762575eddcacf99cca2ba3b591a1b0da7b8c
suthardeepak14 <suthardeepak257@gmail.com>
2024-05-27 13:35:43 +0530

    sidebar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import "./app.css";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+  return (
+    <div className="appContainer">
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
